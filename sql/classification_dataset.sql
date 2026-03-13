a9a03db23de175c4c9c0397159f251fde66db8b4
Kaustubh Kaushal <117793639+kausstubhhh@users.noreply.github.com>
2026-03-13 22:51:08 +0000

    Update classification_dataset.sql

--- sql/classification_dataset.sql
+++ sql/classification_dataset.sql
@@ -9,11 +9,9 @@ record_id,
 height_inches,
 weight_pounds,
 bmi,
-
 CASE
-WHEN bmi >= 30 THEN 1
-ELSE 0
+  WHEN bmi >= 25 THEN 1
+  ELSE 0
 END AS high_bmi
-
 FROM
 `healthcare-analytics-kaust.pharmacy_analytics.prescriptions_features`;
