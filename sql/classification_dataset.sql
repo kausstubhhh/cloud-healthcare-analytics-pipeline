0806e505049381f26cd16607fa39cf17290684d5
Kaustubh Kaushal <117793639+kausstubhhh@users.noreply.github.com>
2026-03-13 22:47:56 +0000

    Create classification_dataset.sql

---
+++ sql/classification_dataset.sql
@@ -0,0 +1,19 @@
+-- Phase 6: Create classification dataset
+
+CREATE OR REPLACE TABLE
+`healthcare-analytics-kaust.pharmacy_analytics.bmi_classification_data`
+AS
+
+SELECT
+record_id,
+height_inches,
+weight_pounds,
+bmi,
+
+CASE
+WHEN bmi >= 30 THEN 1
+ELSE 0
+END AS high_bmi
+
+FROM
+`healthcare-analytics-kaust.pharmacy_analytics.prescriptions_features`;
